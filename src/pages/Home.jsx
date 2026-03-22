7732315d6e70c2c2e2df09f43dae217e73ca5305
Emmanuel Paul <paulemmanuel8888@gmail.com>
2026-03-22 10:22:58 +0100

    Update the Home component to display a loading spinner while fetching crypto data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,25 @@ export const Home = () => {
       setCryptoList(data);
     } catch (error) {
       console.error("Error fetching crypto data: ", error);
+    } finally {
+      setIsLoading(false);
     }
   };
-  return <div className="">Home Page</div>;
+  return (
+    <div className="app">
+      {" "}
+      {isLoading ? (
+        <div className="loading">
+          <div className="spinner"></div>
+          <p>Loading crypto data...</p>
+        </div>
+      ) : (
+        <div className="crypto-container">
+          {cryptoList.map((crypto, key) => (
+            <CryptoCard crypto={crypto} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
