d462372af33536d089c5656d9e6d7d34bf0e359d
Emmanuel Paul <paulemmanuel8888@gmail.com>
2026-03-22 10:04:10 +0100

    Create function to get crypto data using the free coinGecko API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,21 @@
+import { useEffect, useState } from "react";
+import { fetchCryptos } from "../api/coinGecko";
+
 export const Home = () => {
+  const [cryptoList, setCryptoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetchCryptoData();
+  }, []);
+
+  const fetchCryptoData = async () => {
+    try {
+      const data = await fetchCryptos();
+      setCryptoList(data);
+    } catch (error) {
+      console.error("Error fetching crypto data: ", error);
+    }
+  };
   return <div className="">Home Page</div>;
 };
